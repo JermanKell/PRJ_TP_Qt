9dff5103e52317126641fe4cda3f47c20d317b66
arccho <xzeno@orange.fr>
2018-03-17 10:54:16 +0100

    petit up tableview

--- AppliQt/Controleur/checkconnexion.cpp
+++ AppliQt/Controleur/checkconnexion.cpp
@@ -11,17 +11,20 @@ CheckConnexion::~CheckConnexion() {
 }
 
 bool CheckConnexion::CheckCoUser(QString Id, QString mdp) {
-    bool var = false;
+    bool bresult = false;
 
     QSqlQuery query = QSqlQuery(*(Controller_BD::getInstance()->getBD()));
 
     query.prepare("SELECT Login, MdP FROM TCompte WHERE Login LIKE :Id AND MdP LIKE :mdp");
     query.bindValue(":Id", Id);
     query.bindValue(":mdp", mdp);
-    var = query.exec();
 
-    while (query.next())
-        cout << "Connexion réussie, connexion à l'application principale en cours ..." << endl;
+    if(!query.exec()) {
+        qDebug() << query.lastError();
+    }
 
-    return var;
+    if (query.next()) {
+        bresult = true;
+    }
+    return bresult;
 }
--- AppliQt/Vue/mainwindow.cpp
+++ AppliQt/Vue/mainwindow.cpp
@@ -25,6 +25,8 @@ MainWindow::MainWindow(QWidget *parent) :
     QObject::connect(ui->action_client_Tool, SIGNAL(triggered()), this, SLOT(slotAjouterClient()));
     QObject::connect(ui->action_Personnel_Tool, SIGNAL(triggered()), this, SLOT(slotAjouterPersonnel()));
 
+
+    //TableView
     QStandardItemModel * model = new QStandardItemModel(0, 10, this);
     model->setHorizontalHeaderItem(0, new QStandardItem(QString("Id")));
     model->setHorizontalHeaderItem(1, new QStandardItem(QString("Nom")));
@@ -37,6 +39,36 @@ MainWindow::MainWindow(QWidget *parent) :
     model->setHorizontalHeaderItem(8, new QStandardItem(QString("Duree RDV")));
     model->setHorizontalHeaderItem(9, new QStandardItem(QString("Priorite")));
     ui->tableViewClient->setModel(model);
+
+    //Données du TableView
+    QSqlQuery * query = new QSqlQuery(*Controller_BD::getInstance()->getBD());
+    if(!query->exec("SELECT * FROM TClient")) {
+        qDebug() << query->lastError().text();
+    }
+    while (query->next()) {
+        QList<QStandardItem*> newRow;
+        QStandardItem *item_Id = new QStandardItem(query->value(0).toString());
+        QStandardItem *item_Nom = new QStandardItem(query->value(1).toString());
+        QStandardItem *item_Prenom = new QStandardItem(query->value(2).toString());
+        QStandardItem *item_Adresse = new QStandardItem(query->value(3).toString());
+        QStandardItem *item_Ville = new QStandardItem(query->value(4).toString());
+        QStandardItem *item_CP = new QStandardItem(query->value(5).toString());
+        QStandardItem *item_Tel = new QStandardItem(query->value(7).toString());
+        QStandardItem *item_DateRDV = new QStandardItem(query->value(8).toString());
+        QStandardItem *item_DureeRDV = new QStandardItem(query->value(9).toString());
+        QStandardItem *item_Priorite = new QStandardItem(query->value(10).toString());
+        newRow.append(item_Id);
+        newRow.append(item_Nom);
+        newRow.append(item_Prenom);
+        newRow.append(item_Adresse);
+        newRow.append(item_Ville);
+        newRow.append(item_CP);
+        newRow.append(item_Tel);
+        newRow.append(item_DateRDV);
+        newRow.append(item_DureeRDV);
+        newRow.append(item_Priorite);
+        model->appendRow(newRow);
+    }
 }
 
 MainWindow::~MainWindow()
